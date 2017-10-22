5cd03c2a7f989f232d080a941795590bcff35370
Matija Santl <matija.santl@bellabeat.com>
2017-10-22 22:19:56 +0200

    [cryptofolio] Adding statiffiles folder

--- settings.py
+++ settings.py
@@ -122,8 +122,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR, 'cryptofolio/static'),
 )
